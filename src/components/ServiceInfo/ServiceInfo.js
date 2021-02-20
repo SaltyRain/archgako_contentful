e440527e45a04659b0a2aa6865bc82794918c485
Tanya <krivorotovatd@gmail.com>
2021-02-20 17:52:59 +0300

    wip

--- src/components/ServiceInfo/ServiceInfo.js
+++ src/components/ServiceInfo/ServiceInfo.js
@@ -1,16 +1,13 @@
 import React from 'react'
+import parse from 'html-react-parser'
 import './ServiceInfo.scss'
+import {
+  classToServiceName,
+  costOrAddServiceText,
+  parseServiceTableHtml,
+} from './utils'
 
-const classToServiceName = {
-  'a3298215-d88a-58dc-beef-e55edb0c8bc6': 'designerDeparture',
-  '6d413b04-6921-536b-84ce-aadd9c9ac90d': 'complectation',
-  '7a8a1841-7877-5630-9ac8-f10217b0eaec': 'authorSupervision',
-  '332fc7e2-1888-5af9-8554-653871e892a8': 'designProject',
-  '108a3fca-b85f-57ef-bae4-526ad1da1767': 'expressDesign',
-  'c2d57d1b-5c97-5451-9720-1f8d491a70dd': 'consultation',
-}
-
-function Elem({ plan }) {
+const Elem = ({ plan }) => {
   return (
     <button
       ref={squareRef}
@@ -35,59 +32,58 @@ function wrapElem(elem) {
   // return w1 + elem + w2
 }
 
-function parseThead(thead) {
-  const basic = 'Basic',
-    standart = 'Standart',
-    premium = 'Premium'
-
-  let startBasic, endBasic
-  let startStandart, endStandart
-  let startPremium, endPremium
-
-  startBasic = thead.indexOf(basic)
-  endBasic = startBasic + basic.length
-
-  startStandart = thead.indexOf(standart, startBasic)
-  endStandart = startStandart + standart.length
-
-  startPremium = thead.indexOf(premium, startStandart)
-  endPremium = startPremium + premium.length
-
-  let result = ''
-  if (startBasic !== -1) {
-    result =
-      thead.substring(0, startBasic) +
-      wrapElem(basic) +
-      thead.substring(endBasic, startStandart) +
-      wrapElem(standart) +
-      thead.substring(endStandart, startPremium) +
-      wrapElem(premium) +
-      thead.substring(endPremium, thead.length)
-  } else {
-    result =
-      thead.substring(0, startStandart) +
-      wrapElem(standart) +
-      thead.substring(endStandart, startPremium) +
-      wrapElem(premium) +
-      thead.substring(endPremium, thead.length)
-  }
-  return result
+function parseTrAddThead(trHTML) {
+  return (
+    <thead
+      dangerouslySetInnerHTML={{
+        __html: trHTML,
+      }}
+    ></thead>
+  )
 }
 
 function parseHtml(html) {
   // Если в блоке есть таблица
-  let result
   if (html.indexOf('<thead>') !== -1) {
-    const start = html.indexOf('<thead>')
-    const end = html.indexOf('</thead>') + 8
-    let thead = html.substring(start, end)
-    let newHead = parseThead(thead)
-    result =
-      html.substring(0, start) + newHead + html.substring(end, html.length)
-  } else {
-    result = html
-  }
-  return result
+    const start = html.indexOf('<tr>')
+    const end = html.indexOf('</tr>') + 5
+    const tr = html.substring(start, end)
+    console.log(tr, 'thead')
+    const newHead = parseTrAddThead(tr)
+    console.log(newHead, 'newHead')
+  } else return html
+}
+
+const ServicePrice = ({ price, lang }) => {
+  return (
+    <div className="service-info--price-block">
+      <div className="service-info-rect disabled"></div>
+      <p className="service-info--text">{costOrAddServiceText[lang].price}</p>
+      <p className="service-info--price">{price}</p>
+
+      <button className="button service-info--button">
+        {costOrAddServiceText[lang].add}
+      </button>
+    </div>
+  )
+}
+
+const ServiceButton = ({ lang }) => {
+  return (
+    <div className="service-info--button_float-right">
+      <button className="button service-info--button">
+        {costOrAddServiceText[lang].add}
+      </button>
+    </div>
+  )
+}
+
+const getServiceInfoHtml = (html) => {
+  return (
+    <div className="service-info--wrapper">
+      {html.includes('thead') ? parseServiceTableHtml(html) : parse(html)}
+    </div>
+  )
 }
 
 function ServiceInfo({ group, data, lang, activeInfo }) {
@@ -102,65 +98,38 @@ function ServiceInfo({ group, data, lang, activeInfo }) {
     // console.log(square, 'square')
   }, [])
 
-  let newArray = data.filter((elem) => {
-    if (elem.group === group) {
-      return elem
-    }
-  })
-
-  let constText = []
-  if (lang === 'ru') {
-    constText[0] = 'Стоимость услуги'
-    constText[1] = 'Добавить услугу'
-  } else {
-    constText[0] = 'Service cost'
-    constText[1] = 'Add service'
-  }
-
-  let servicePrice = (price) => {
-    return (
-      <div className="service-info--price-block">
-        <div className="service-info-rect disabled"></div>
-        <p className="service-info--text">{constText[0]}</p>
-        <p className="service-info--price">{price}</p>
-
-        <button className="button service-info--button">{constText[1]}</button>
-      </div>
-    )
-  }
-  let serviceButton = (
-    <div className="service-info--button_float-right">
-      <button className="button service-info--button">{constText[1]}</button>
-    </div>
+  const servicesByGroup = data.filter(
+    (serviceItem) => serviceItem.group === group
   )
 
-  const getActiveByGroup = () => {
-    return group === 'main' ? 'active-main' : 'active-extra'
-  }
+  const getClassPropertyByGroup = () =>
+    group === 'main' ? 'active-main' : 'active-extra'
 
   return (
     <>
-      {newArray.map((item) => {
-        const parsedHTML = parseHtml(item.body.childMarkdownRemark.html)
+      {servicesByGroup.map((service) => {
         return (
           <div
-            key={item.id}
+            key={service.id}
             className={
               `${
-                item.price ? 'service-info' : 'service-info service-info_block'
+                service.price
+                  ? 'service-info'
+                  : 'service-info service-info_block'
               }` +
               ' ' +
-              `${activeInfo === item.id ? getActiveByGroup() : 'hide'}`
+              `${
+                activeInfo === service.id ? getClassPropertyByGroup() : 'hide'
+              }`
             }
-            id={item.id}
+            id={service.id}
           >
-            <div
-              className="service-info--wrapper"
-              dangerouslySetInnerHTML={{
-                __html: parsedHTML,
-              }}
-            />
-            {item.price ? servicePrice(item.price) : serviceButton}
+            {getServiceInfoHtml(service.body.childMarkdownRemark.html)}
+            {service.price ? (
+              <ServicePrice price={service.price} lang={lang} />
+            ) : (
+              <ServiceButton lang={lang} />
+            )}
           </div>
         )
       })}
---
+++ src/components/ServiceInfo/utils.js
@@ -0,0 +1,21 @@
+export const classToServiceName = {
+  'a3298215-d88a-58dc-beef-e55edb0c8bc6': 'designerDeparture',
+  '6d413b04-6921-536b-84ce-aadd9c9ac90d': 'complectation',
+  '7a8a1841-7877-5630-9ac8-f10217b0eaec': 'authorSupervision',
+  '332fc7e2-1888-5af9-8554-653871e892a8': 'designProject',
+  '108a3fca-b85f-57ef-bae4-526ad1da1767': 'expressDesign',
+  'c2d57d1b-5c97-5451-9720-1f8d491a70dd': 'consultation',
+}
+
+export const costOrAddServiceText = {
+  ru: {
+    price: 'Стоимость услуги',
+    add: 'Добавить услугу',
+  },
+  en: {
+    price: 'Service cost',
+    add: 'Add service',
+  },
+}
+
+export const parseServiceTableHtml = (serviceHtml) => {}
